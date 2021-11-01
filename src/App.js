5f4fa67f76a762f19c7a71687236372d34785797
SlavikChuprin <chuprin.00@yandex.ru>
2021-11-01 22:51:06 +0300

    add gh-pages packeg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { Component } from 'react';
 import './App.css';
 import Searchbar from './components/Searchbar';
-function App() {
+
+
+class App extends Component {
+
+  render(){}
   return (
     <div className="App">
       <Searchbar />
